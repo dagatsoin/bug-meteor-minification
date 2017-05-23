395daf916ea1dd1ff44a7664df1bde62a3c9ce3e
daniel <daniel.neveux@gmail.com>
2017-05-23 21:03:26 +0200

    fix repro

--- imports/Map.jsx
+++ imports/Map.jsx
@@ -13,67 +13,7 @@ class Map extends Component {
       container: findDOMNode(this.refs.map),
       style: 'mapbox://styles/mapbox/streets-v8',
     })
-
-    this.map.doubleClickZoom.disable()
-
-    if (this.props.zoom) {
-      this.map.setZoom(this.props.zoom)
-    }
-
-    if (this.props.center) {
-      this.map.setCenter(this.props.center)
-    }
-
-    if (this.props.onMove) {
-      this.map.on('move', () => {
-        this.props.onMove(this.map.getCenter().toArray(), this.map.getZoom(), this.map.getBounds().toArray())
-      })
-    }
-
-    if (this.props.onDoubleClick) {
-      this.map.on('dblclick', (event) => {
-        this.props.onDoubleClick(event.lngLat.lng, event.lngLat.lat)
-      })
-    }
-
-    this.map.on('load', () => {
-      this.loaded = true
-
-      this.map.addSource('data', {
-        type: 'geojson',
-        data: this.props.data,
-      })
-
-      this.map.addLayer({
-        id: 'data',
-        type: 'symbol',
-        source: 'data',
-        layout: {
-          'icon-image': '{symbol}-15',
-          'text-field': '{title}',
-          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-          'text-offset': [0, 0.6],
-          'text-anchor': 'top',
-        },
-      })
-    })
   }
-
-  componentWillUnmount() {
-    this.map.remove()
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!this.loaded || _.isEqual(prevProps.data, this.props.data)) {
-      return
-    }
-
-    this.map.getSource('data').setData(this.props.data)
-  }
-}
-
-Map.propTypes = {
-  data: PropTypes.object.isRequired,
 }
 
 export default Map
